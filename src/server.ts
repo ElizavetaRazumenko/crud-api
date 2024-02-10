7ce78712b21c1d20f469371941e89d2648c4f634
ElizavetaRazumenko <razumenko99@mail.ru>
2024-02-10 23:57:04 +0300

    feat: add server basic settings

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-/* server */
+import http, { Server } from 'http';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+const port = process.env.NODE_PORT;
+
+export const server: Server = http.createServer();
+
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
