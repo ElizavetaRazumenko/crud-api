9242c06dae06d235106cd3d020342cf3c21024ca
ElizavetaRazumenko <razumenko99@mail.ru>
2024-02-11 15:17:11 +0300

    feat: clear the main branch

--- src/constants/constants.ts
+++
@@ -1,1 +0,0 @@
-export const MAX_ENDPOINT_DEEP = 4;
--- src/tools/sendOptionsResponse.ts
+++
@@ -1,9 +0,0 @@
-import { ServerResponse } from 'http';
-
-export const sendOptionsResponse = (response: ServerResponse) => {
-  response.setHeader('Access-Control-Allow-Origin', '*');
-  response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, DELETE'); 
-  response.setHeader('Access-Control-Allow-Headers', 'Content-Type'); 
-  response.writeHead(204);
-  response.end();
-};
